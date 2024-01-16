4ede011472d5fe13f7b3a6308f5ce0bc49f86496
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-16 18:49:54 +0100

    feat: nuovi prodotti + immagini

--- database/db.php
+++ database/db.php
@@ -5,25 +5,50 @@ $database = [
     [
         "titolo" => "Carnilove Snack Croccante Salmone",
         "prezzo" => 11.40,
-        "immagine" => "Immagine Snack Croccante Salmone",
+        "immagine" => "img/snack-salmone.webp",
         "animale" => "Cane",
-        "iconaAnimale" => "Icona Cane",
+        "iconaAnimale" => "img/dog-solid.svg",
         "articolo" => "Cibo"
     ],
     [
         "titolo" => "Corda Multicolore",
         "prezzo" => 2.19,
-        "immagine" => "Immagine Corda Multicolore",
+        "immagine" => "img/corda-multicolore.jpg",
         "animale" => "Gatto",
-        "iconaAnimale" => "Icona Gatto",
+        "iconaAnimale" => "img/cat-solid.svg",
         "articolo" => "Gioco"
     ],
     [
         "titolo" => "Cuccia Per Cani Verde",
         "prezzo" => 68.99,
-        "immagine" => "Immagine Cuccia Per Cani",
+        "immagine" => "img/cuccia-verde.webp",
         "animale" => "Cane",
-        "iconaAnimale" => "Icona Cane",
+        "iconaAnimale" => "img/dog-solid.svg",
         "articolo" => "Cuccia"
     ],
+    [
+        "titolo" => "Palla Multicolore",
+        "prezzo" => 28.35,
+        "immagine" => "img/palla-multicolore.jpg",
+        "animale" => "Cane",
+        "iconaAnimale" => "img/dog-solid.svg",
+        "articolo" => "Gioco"
+    ],
+    [
+        "titolo" => "Purina Salmone",
+        "prezzo" => 10.69,
+        "immagine" => "img/cibo-purina.jpg",
+        "animale" => "Gatto",
+        "iconaAnimale" => "img/cat-solid.svg",
+        "articolo" => "Cibo"
+    ],
+    [
+        "titolo" => "Animonda Carne di Vitello",
+        "prezzo" => 2.18,
+        "immagine" => "img/cibo-animonda.jpg",
+        "animale" => "Gatto",
+        "iconaAnimale" => "img/cat-solid.svg",
+        "articolo" => "Cibo"
+    ],
+
 ];
