e1ec99dc18bce532f08375effc0330810e546e49
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-16 17:29:42 +0100

    feat: database prodotti

---
+++ database/db.php
@@ -0,0 +1,29 @@
+<?php
+
+// database prodotti
+$database = [
+    [
+        "titolo" => "Carnilove Snack Croccante Salmone",
+        "prezzo" => 11.40,
+        "immagine" => "Immagine Snack Croccante Salmone",
+        "animale" => "Cane",
+        "iconaAnimale" => "Icona Cane",
+        "articolo" => "Cibo"
+    ],
+    [
+        "titolo" => "Corda Multicolore",
+        "prezzo" => 2.19,
+        "immagine" => "Immagine Corda Multicolore",
+        "animale" => "Gatto",
+        "iconaAnimale" => "Icona Gatto",
+        "articolo" => "Gioco"
+    ],
+    [
+        "titolo" => "Cuccia Per Cani Verde",
+        "prezzo" => 68.99,
+        "immagine" => "Immagine Cuccia Per Cani",
+        "animale" => "Cane",
+        "iconaAnimale" => "Icona Cane",
+        "articolo" => "Cuccia"
+    ],
+];
